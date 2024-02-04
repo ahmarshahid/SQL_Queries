5931f481969c4c8cc63ae7982e6da8ad2af51865
ahmarshahid <120157630+ahmarshahid@users.noreply.github.com>
2024-02-04 17:35:52 +0500

    Add files via upload

---
+++ DBscript.sql
@@ -0,0 +1,78 @@
+/* Question no : 1*/
+/* Script time 2.46 PM */
+/* Script date 02-02-24 */
+/*Select * from Orders where RequiredDate < ShippedDate */
+
+/* Question no : 2*/
+/* Script time 2.48 PM */
+/* Script date 02-02-24 */
+/*Select FirstName, Country from Employees */
+
+
+/* Question no : 3*/
+/* Script time 2.55 PM */
+/* Script date 02-02-24 */
+Select * from Employees where ReportsTo IS NUll
+
+
+
+/* Question no : 4*/
+/* Script time 3.15 PM */
+/* Script date 02-02-24 */
+/*Select ProductName from products where Discontinued = 1*/
+
+
+/* Question no : 5*/
+/* Script time 3.22 PM */
+/* Script date 02-02-24 */
+/*Select OrderID from "Order Details" where discount = 0*/
+
+/* Question no : 6*/
+/* Script time 3.43 PM */
+/* Script date 02-02-24 */
+/*Select ContactName from Customers where Region IS Null*/
+
+/* Question no : 7*/
+/* Script time 5.01 PM */
+/* Script date 02-02-24 */
+/*Select ContactName,Phone from Customers Where country = 'UK' OR country = 'USA'*/
+
+/* Question no : 8*/
+/* Script time 5.01 PM */
+/* Script date 02-02-24 */
+/*Select CompanyName from Suppliers where Homepage IS Null*/
+
+/* Question no : 9*/
+/* Script time 5.10 PM */
+/* Script date 02-02-24 */
+/*Select ShipCountry from Orders where ShippedDate >= '1997-01-01 00:00:00' AND ShippedDate > '1998-01-01 00:00:00'*/
+
+/* Question no : 10*/
+/* Script time 5.29 PM */
+/* Script date 02-02-24 */
+/*Select CustomerID from Orders Where ShippedDate IS NUll*/
+
+/* Question no : 11*/
+/* Script time 5.32 PM */
+/* Script date 02-02-24 */
+/*Select SupplierID,CompanyName,City from Suppliers*/
+
+/* Question no : 12*/
+/* Script time 7.13 PM */
+/* Script date 02-02-24 */
+/*Select FirstName,LastName from Employees where City = 'London'*/
+
+/* Question no : 13*/
+/* Script time 7.18 PM */
+/* Script date 02-02-24 */
+/*Select ProductName from products where Discontinued = 0*/
+
+/* Question no : 14*/
+/* Script time 7.21 PM */
+/* Script date 02-02-24 */
+/*SELECT OrderID from "Order Details" where Discount <= 0.1*/
+
+/* Question no : 15*/
+/* Script time 7.29 PM */
+/* Script date 02-02-24 */
+/*SELECT EmployeeID,FirstName,LastName,HomePhone,Extension from Employees Where Region IS Null*/
